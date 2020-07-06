64d38267c0ef368f6ae1b3252aaba56d9655343a
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 13:51:01 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -47,7 +47,7 @@ def add_characters
          ]
        },
        :additional_characters => [
-         {:name => "Prince Escalus"}, {name: => "Apothecary"}
+         {:name => "Prince Escalus"}, {:name => "Apothecary"}
        ]
        }
        }
